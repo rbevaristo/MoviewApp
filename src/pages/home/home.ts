d2e3b23eed497adb63266cb0011a0f59b7003db0
Richard Evaristo <r.evaristo@coi.local>
2018-12-19 13:14:08 +0800

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Http } from "@angular/http";
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  constructor(public navCtrl: NavController, public http: Http) {}
+  public videos: any;
 
-  constructor(public navCtrl: NavController) {
-
+  ionViewDidLoad() {
+    this.http
+      .get("http://192.168.2.144:8000/api/movies")
+      .subscribe(
+        data => this.handleResponse(data),
+        error => this.handleError(error)
+      );
   }
 
+  handleResponse(data) {
+    console.log(JSON.parse(data._body));
+    this.videos = JSON.parse(data._body).data;
+    console.log(this.videos);
+  }
+
+  handleError(error) {
+    console.log(error);
+  }
 }
